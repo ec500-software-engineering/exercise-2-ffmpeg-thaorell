6e0e25baf4b16ffeba4fef1b87635490a0ea4dbf
thaorell <khanhchuong7396@gmail.com>
2019-02-25 17:49:54 -0500

    added mechanism to stop thread

--- compute.py
+++ compute.py
@@ -9,16 +9,16 @@ out = './processed/'
 def process(Q,path):
     while not Q.empty():
         video = Q.get()
-        print ("length of this video before processing is " + str(length.getLen(video)))
+        print ("length of " + video + " before processing is " + str(length.getLen(video)))
     
         vid_720p = 'ffmpeg -i '+ video + ' -s hd720 ' + video[:-4] + '_720p.mp4'
         name = video[len(vids_path):] #list of names
         subprocess.call(vid_720p, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
         print (name + ' 720p done')
-        print ("length of this video after converting to 720p is " + str(length.getLen(video[:-4] + '_720p.mp4')))
+        print ("length of " + video + " after 720p processing is " + str(length.getLen(video[:-4] + '_720p.mp4')))
 
         vid_480p = 'ffmpeg -i '+ video +' -s hd480 '+video[:-4] + '_480p.mp4'
         subprocess.call(vid_480p, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
         print (name + ' 480p done')
-        print ("length of this video after converting to 480p is " + str(length.getLen(video[:-4] + '_480p.mp4')))
+        print ("length of " + video + " after 480p processing is " + str(length.getLen(video[:-4] + '_480p.mp4')))
         return length.getLen(video)
--- ffmpeg.py
+++ ffmpeg.py
@@ -7,12 +7,13 @@ out = './processed'
 
 def main():
 	q = queue.Queue()
-	folder = os.listdir(vids_path)
-	for file in folder:
+	directory = os.listdir(vids_path)
+	for file in directory:
 		q.put(vids_path+"/"+file)
 	for i in range(3):
 		worker = threading.Thread(target=compute.process, args=(q,vids_path))
 		worker.start()
+		worker.join()
 
 if __name__ == "__main__":
 	main()
