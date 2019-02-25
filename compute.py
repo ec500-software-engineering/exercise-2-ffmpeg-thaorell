0e9e5fba6eaffd68a4baeb6f108c00ce4456bfc0
thaorell <khanhchuong7396@gmail.com>
2019-02-25 18:10:06 -0500

    debug info in compute

--- compute.py
+++ compute.py
@@ -9,16 +9,24 @@ out = './processed/'
 def process(Q,path):
     while not Q.empty():
         video = Q.get()
-        print ("length of " + video + " before processing is " + str(length.getLen(video)))
+        original_len = length.getLen(video)
+        print ("length of " + video + " before processing is " + str(original_len))
     
         vid_720p = 'ffmpeg -i '+ video + ' -s hd720 ' + video[:-4] + '_720p.mp4'
         name = video[len(vids_path):] #list of names
         subprocess.call(vid_720p, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
         print (name + ' 720p done')
-        print ("length of " + video + " after 720p processing is " + str(length.getLen(video[:-4] + '_720p.mp4')))
+        len_720 = length.getLen(video[:-4] + '_720p.mp4')
+        print ("length of " + video + " after 720p processing is " + str(len_720))
+        if len_720 != original_len: 
+            raise Exception("Processing error. Length mismatched")
 
         vid_480p = 'ffmpeg -i '+ video +' -s hd480 '+video[:-4] + '_480p.mp4'
         subprocess.call(vid_480p, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
         print (name + ' 480p done')
-        print ("length of " + video + " after 480p processing is " + str(length.getLen(video[:-4] + '_480p.mp4')))
+        len_480 = length.getLen(video[:-4] + '_480p.mp4')
+        print ("length of " + video + " after 480p processing is " + str(len_480))
+        if len_480 != original_len: 
+            raise Exception("Processing error. Length mismatched")
+
         return length.getLen(video)
